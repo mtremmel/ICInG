df5b10ff6cd3207051ae517b82040003430d72d1
Michael Tremmel <mjt29@hyades.astro.washington.edu>
2014-08-21 15:55:50 -0700

    added LICENSE and README file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+DESCRIPTION = 'Tools for creating initial conditions for an isolated cosmological collapse simulation '
+LONG_DESCRIPTION = open('README.md').read()
+NAME = 'pyCosmoColl'
+VERSION = '1.0'
+AUTHOR = 'Michael Tremmel'
+AUTHOR_EMAIL = 'm.tremmel6@gmail.com'
+MAINTAINER = 'Michael Tremmel'
+MAINTAINER_EMAIL = 'm.tremmel6@gmail.com'
+URL = 'http://github.com/mtremmel/pyHoles'
+DOWNLOAD_URL = 'http://github.com/mtremmel/pyHoles'
+LICENSE = 'BSD'
+
+
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR, 
+      author_email= AUTHOR_EMAIL,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      url=URL,
+      download_url=DOWNLOAD_URL,
+      license=LICENSE,
+      package_dir={'pyCosmoColl/':''},
+      packages=['pyCosmoColl'], 
+      package_data={'pyCosmoColl':['data/snapshot_005.hdf5']},
+      classifiers = ["Development Status :: 3 - Alpha",
+                     "Intended Audience :: Developers",
+                     "Intended Audience :: Science/Research",
+                     "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+                     "Programming Language :: Python :: 2",
+                     "Topic :: Scientific/Engineering :: Astronomy"])
